0bad9ab6abbf3390e1f5401f703a3e84e453a4f6
Halim Madjid <halimmadjid@gmail.com>
2021-02-07 14:41:04 -0500

    first commit

---
+++ marvel_api/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from config import Config
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from flask_login import LoginManager
+
+from authlib.integrations.flask_client import OAuth
+
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+login_manager = LoginManager(app)
+login_manager.login_view = 'signin'
+
+oauth = OAuth(app)
+
+ma = Marshmallow(app)
+
+from marvel_api import routes, models
+
